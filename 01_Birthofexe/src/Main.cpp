b016a3b95342ffbd56ca273febda6fae8d6fd149
jeganitis <72639182+jeganitis@users.noreply.github.com>
2026-02-02 00:10:34 +0530

    Delete 01_Birthofexe directory

--- 01_Birthofexe/src/Main.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <iostream>
-#include "Middleware.h"
-int main() {
-    std::cout << "--- C++ Middleware Example ---\n";
-    Middleware mw;
-    mw.processRequest("SensorDataRequest");
-    return 0;
-}
