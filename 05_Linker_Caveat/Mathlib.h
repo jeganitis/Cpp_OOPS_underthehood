52dc3591a18b5272da67905d8a758819ddca3ee7
jeganitis <72639182+jeganitis@users.noreply.github.com>
2026-02-02 00:12:30 +0530

    Add files via upload

---
+++ 05_Linker_Caveat/Mathlib.h
@@ -0,0 +1,7 @@
+#ifndef MATHLIB_H
+#define MATHLIB_H
+
+int add(int, int);
+int multiply(int, int);
+
+#endif
---
+++ 05_Linker_Caveat/newmain.c
@@ -0,0 +1,9 @@
+#include "Mathlib.h"
+#include<stdio.h>
+
+int main(int argc, char** argv) {
+ int x = add(4, 5);
+ printf("Sum is :%d",x);
+
+ return 0;
+}
